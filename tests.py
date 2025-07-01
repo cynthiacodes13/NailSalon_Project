9bcd6a62169583f42c5893e982f14165cf0b4dd8
Cynthia Ordaz <ordaz0877@gmail.com>
2025-06-30 18:49:08 -0700

    Update tests.py
    update tests

--- tests.py
+++ tests.py
@@ -1,1 +1,19 @@
+import unittest
+import server
 
+class FlaskTests(unittest.TestCase):
+
+    def setUp(self):
+
+        self.client = server.app.test_client()
+        server.app.config['TESTING'] = True
+    
+    def test_homepage(self):
+
+        result = self.client.get("/")
+        self.assertIn(b"Welcome",result.data)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
